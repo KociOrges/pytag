f6cefa1a7535907e5f78db7b8a69f66a70855d67
KociOrges <30604050+KociOrges@users.noreply.github.com>
2017-10-25 01:00:35 +0100

    Update __init__.py

--- pytag/__init__.py
+++ pytag/__init__.py
@@ -1,14 +1,14 @@
 b'This module needs Python 3.4.0 or newer'
 
 # Special variables #
-__version__ = '1.4'
+__version__ = '1.0.0'
 version_string = "trytouploadhelloworld version %s" % __version__
 
 
 # Internal modules #
 __all__ = ['analysis', 'annotation', 'ontologies']
-from trytouploadhelloworld.analysis import process
-from trytouploadhelloworld.annotation import annotate_abstract
-from trytouploadhelloworld.ontologies import extract_terms
+from pytag.analysis import process
+from pytag.annotation import annotate_abstract
+from pytag.ontologies import extract_terms
 
 
