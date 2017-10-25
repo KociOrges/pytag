f3c17745e791cadff019efc657c00589506d19e1
KociOrges <30604050+KociOrges@users.noreply.github.com>
2017-10-25 16:19:03 +0100

    Add files via upload

---
+++ pytag/__init__.py
@@ -0,0 +1,12 @@
+b'This module needs Python 3.4.0 or newer'
+
+# Special variables #
+__version__ = '1.0.0'
+version_string = "pytag version %s" % __version__
+
+
+# Internal modules #
+__all__ = ['analysis', 'annotation', 'ontologies']
+from trytouploadhelloworld.analysis import process
+from trytouploadhelloworld.annotation import *
+from trytouploadhelloworld.ontologies import *
