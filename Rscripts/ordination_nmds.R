cbd730700111697ba3beb21cb5a4498628c03a77
Orges Koci <koci.orges@hotmail.com>
2022-10-13 16:06:14 +0100

    Update ordination_nmds.R

--- Rscripts/ordination_nmds.R
+++ Rscripts/ordination_nmds.R
@@ -1,5 +1,5 @@
 ordination_nmds <- function(freq_data, meta_table) {
-  
+  # Test line
   # perform NDMS using 'bray-curtis' distance
   sol <- metaMDS(freq_data, distance = "bray", k = 2, trymax = 50)
   
